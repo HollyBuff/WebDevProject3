fd95fdc80d493ce2b4c085d9b83587b5e98c44e3
StevenQin1996 <sxq13a@acu.edu>
2017-04-19 13:34:32 -0500

    stuff

---
+++ game.php
@@ -0,0 +1,398 @@
+<?php
+session_start ();
+
+if(isset($_SESSION['use']))   // Checking whether the session is already there or not if                            // true then header redirect it to the home page directly 
+ {
+    header("Location:Initial.html"); 
+ }
+ else 
+  if ($_SESSION["pass_userName"] != null)
+  {
+     echo "<script>alert('Welcome Back,".$_SESSION["pass_userName"]."');</script>";
+     echo 'Welcome Back  ', $_SESSION["pass_userName"];
+  }
+  
+function loginForm() {
+    echo '
+   <div id="loginform">
+   <form action="game.php" method="post">
+       <p>Please enter your name to continue:</p>
+       <label for="name">Name:</label>
+       <input type="text" name="name" id="name" />
+       <input type="submit" name="enter" id="enter" value="Enter" />
+   </form>
+   </div>
+   ';
+}
+ 
+if (isset ( $_POST ['enter'] )) {
+    if ($_POST ['name'] != "") {
+        $_SESSION ['name'] = stripslashes ( htmlspecialchars ( $_POST ['name'] ) );
+        $fp = fopen ( "log.html", 'a' );
+        fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has joined the chat session.</i><br></div>" );
+        fclose ( $fp );
+    } else {
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+ 
+if (isset ( $_GET ['logout'] )) {
+   
+    // Simple exit message
+    $fp = fopen ( "log.html", 'a' );
+    fwrite ( $fp, "<div class='msgln'><i>User " . $_SESSION ['name'] . " has left the chat session.</i><br></div>" );
+    fclose ( $fp );
+   
+    session_destroy ();
+    header ( "Location: game.php" ); // Redirect the user
+}
+ 
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<style>
+canvas { background: #00FFFF; display: block; margin: 0px; }
+body {
+    font: 12px arial;
+    color: #222;
+    text-align: center;
+    padding: 35px;
+}
+ 
+form,p,span {
+    margin: 0;
+    padding: 0;
+}
+ 
+input {
+    font: 12px arial;
+}
+ 
+a {
+    color: #0000FF;
+    text-decoration: none;
+}
+ 
+a:hover {
+    text-decoration: underline;
+}
+ 
+#wrapper,#loginform {
+    margin: 0px;
+    margin-top: 10px;
+    padding-bottom: 25px;
+    background: #EBF4FB;
+    width: 500px;
+    border: 1px solid #ACD8F0;
+    float: left;
+}
+ 
+#loginform {
+    padding-top: 18px;
+}
+ 
+#loginform p {
+    margin: 5px;
+}
+ 
+#chatbox {
+    text-align: left;
+    margin: 0 auto;
+    margin-bottom: 25px;
+    padding: 10px;
+    background: #fff;
+    height: 270px;
+    width: 430px;
+    border: 1px solid #ACD8F0;
+    overflow: auto;
+}
+ 
+#usermsg {
+    width: 395px;
+    border: 1px solid #ACD8F0;
+}
+ 
+#submit {
+    width: 60px;
+}
+ 
+.error {
+    color: #ff0000;
+}
+ 
+#menu {
+    padding: 12.5px 25px 12.5px 25px;
+}
+ 
+.welcome {
+    float: left;
+}
+ 
+.logout {
+    float: right;
+}
+ 
+.msgln {
+    margin: 0 0 2px 0;
+}
+</style>
+<title>Chat - Customer Module</title>
+</head>
+<body>
+
+    <canvas id="myCanvas" width="960" height="640"></canvas>
+
+<script>
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var ballRadius = 10;
+    var x = canvas.width/2; // start off possition.
+    var y = canvas.height-70; // start off position
+    var dx = 5; // control gravity / dropping angel and speed
+    var dy = -5;// control gravity / dropping angel and speed
+    var paddleHeight = 10;
+    var paddleWidth = 75;
+    var paddleX = (canvas.width-paddleWidth)/2;//space outside the paddle in half
+    var rightPressed = false;//mouse will move to right automatically when user is not controlling
+    var leftPressed = false;//mouse will move to left automatically when user is not controlling.
+    var brickRowCount = 14;
+    var brickColumnCount = 8;
+    var brickWidth = 60; 
+    var brickHeight = 15; 
+    var brickPadding = 8;//gap in between each brick
+    var brickOffsetTop = 30;
+    var brickOffsetLeft = 10;
+    var score = 0;
+    var lives = 3;
+
+    var bricks = [];
+    for(c=0; c<brickColumnCount; c++) {
+        bricks[c] = [];
+        for(r=0; r<brickRowCount; r++) {
+            bricks[c][r] = { x: 0, y: 0, status: 1 };
+        }
+    }
+
+    document.addEventListener("keydown", keyDownHandler, false);
+    document.addEventListener("keyup", keyUpHandler, false);
+    document.addEventListener("mousemove", mouseMoveHandler, true);
+
+    function keyDownHandler(e) { // let key board to control the paddle
+        if(e.keyCode == 39) {
+            rightPressed = true;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = true;
+        }
+    }
+    function keyUpHandler(e) { // let key board to control the paddle
+        if(e.keyCode == 39) {
+            rightPressed = false;
+        }
+        else if(e.keyCode == 37) {
+            leftPressed = false;
+        }
+    }
+    function mouseMoveHandler(e) {
+        var relativeX = e.clientX - canvas.offsetLeft;
+        if(relativeX > 0 && relativeX < canvas.width) {
+            paddleX = relativeX - paddleWidth/2;
+        }
+    }
+    function collisionDetection() {
+        for(c=0; c<brickColumnCount; c++) {
+            for(r=0; r<brickRowCount; r++) {
+                var b = bricks[c][r];
+                if(b.status == 1) {
+                    if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) { // if hit the brick 
+                        dy = -dy; // control the ball to go the oposit direction
+                        b.status = 0;
+                        score++;
+                        if(score == brickRowCount*brickColumnCount) { //every time you hit a brick, your score increase by 1.
+                            alert("YOU WIN, CONGRATS!");
+                            document.location.reload();
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+        ctx.fillStyle = "#BC8F8F";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawBricks() {
+        for(c=0; c<brickColumnCount; c++) {
+            for(r=0; r<brickRowCount; r++) {
+                if(bricks[c][r].status == 1) {
+                    var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                    var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                    ctx.fillStyle = "#A52A2A";
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
+    }
+    function drawScore() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText("Score: "+score, 8, 20);
+    }
+    function drawLives() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+    }
+
+    function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBricks();
+        drawBall();
+        drawPaddle();
+        drawScore();
+        drawLives();
+        collisionDetection();
+
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+        }
+        if(y + dy < ballRadius) {
+            dy = -dy;
+        }
+        else if(y + dy > canvas.height-ballRadius) {
+            if(x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+            }
+            else {
+                lives--;
+                if(lives == -1) {
+                    alert("GAME OVER");
+                    document.location.reload();
+                }
+                else {
+                    x = canvas.width/2;
+                    y = canvas.height-70;
+                    dx = 5;
+                    dy = -5;
+                    paddleX = (canvas.width-paddleWidth)/2;
+                }
+            }
+        }
+
+        if(rightPressed && paddleX < canvas.width-paddleWidth) {
+            paddleX += 7;
+        }
+        else if(leftPressed && paddleX > 0) {
+            paddleX -= 7;
+        }
+
+        x += dx;
+        y += dy;
+        requestAnimationFrame(draw);
+    }
+
+    draw();
+</script>
+    <?php
+    if (! isset ( $_SESSION ['name'] )) {
+        loginForm ();
+    } else {
+        ?>
+<div id="wrapper">
+        <div id="menu">
+            <p class="welcome">
+                Welcome, <b><?php echo $_SESSION['name']; ?></b>
+            </p>
+            <p class="logout">
+                <a id="exit" href="#">Exit Chat</a>
+            </p>
+            <div style="clear: both"></div>
+        </div>
+        <div id="chatbox"><?php
+        if (file_exists ( "log.html" ) && filesize ( "log.html" ) > 0) {
+            $handle = fopen ( "log.html", "r" );
+            $contents = fread ( $handle, filesize ( "log.html" ) );
+            fclose ( $handle );
+           
+            echo $contents;
+        }
+        ?></div>
+ 
+        <form name="message" action="">
+            <input name="usermsg" type="text" id="usermsg" size="63" /> <input
+                name="submitmsg" type="submit" id="submitmsg" value="Send" />
+        </form>
+    </div>
+    <script type="text/javascript"
+        src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+    <script type="text/javascript">
+// jQuery Document
+$(document).ready(function(){
+});
+ 
+//jQuery Document
+$(document).ready(function(){
+    //If user wants to end session
+    $("#exit").click(function(){
+        var exit = confirm("Are you sure you want to end the session?");
+        if(exit==true){window.location = 'game.php?logout=true';}     
+    });
+});
+ 
+//If user submits the form
+$("#submitmsg").click(function(){
+        var clientmsg = $("#usermsg").val();
+        $.post("post.php", {text: clientmsg});             
+        $("#usermsg").attr("value", "");
+        loadLog;
+    return false;
+});
+ 
+function loadLog(){    
+    var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+    $.ajax({
+        url: "log.html",
+        cache: false,
+        success: function(html){       
+            $("#chatbox").html(html); //Insert chat log into the #chatbox div  
+           
+            //Auto-scroll          
+            var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+            if(newscrollHeight > oldscrollHeight){
+                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+            }              
+        },
+    });
+}
+ 
+setInterval (loadLog, 2500);
+
+
+</script>
+<?php
+    }
+    ?>
+    <script type="text/javascript"
+        src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+    <script type="text/javascript">
+</script>
+
+</body>
+</html>
