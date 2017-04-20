d0c71a1be84a9a6909f4b14361dff2bc0d2208a8
Holly <hab13a@acu.edu>
2017-04-19 19:52:24 -0500

    play pause button

--- game.php
+++ game.php
@@ -100,6 +100,7 @@
 <body>
 
     <canvas id="myCanvas" width="960" height="640"></canvas>
+    <button type="button">Play/Pause</button>
 
         <?php 
     $colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
