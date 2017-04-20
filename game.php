c906f3af5cdb786c8741db6579c2eae1c51bff62
Holly <hab13a@acu.edu>
2017-04-19 21:18:02 -0500

    Joe Works!!!!!!

--- game.php
+++ game.php
@@ -10,6 +10,7 @@
     {
         echo "<script>alert('Welcome Back,".$_SESSION["pass_userName"]."');</script>";
         echo 'Welcome Back  ', $_SESSION["pass_userName"];
+        $name = $_SESSION["pass_userName"];
     }
 
  
@@ -102,6 +103,18 @@
     <canvas id="myCanvas" width="960" height="640"></canvas>
     <button type="button">Play/Pause</button>
 
+    <div class="chat_wrapper">
+    <div class="message_box" id="message_box"></div>
+    <div class="panel">
+    <input type="text" name="name" id="name" placeholder="Your Name" maxlength="15" />
+
+    <input type="text" name="message" id="message" placeholder="Message" maxlength="80" 
+    onkeydown = "if (event.keyCode == 13)document.getElementById('send-btn').click()"  />
+
+    </div>
+        <button id="send-btn" class=button>Send</button>
+    </div>
+
         <?php 
     $colours = array('007AFF','FF7000','FF7000','15E25F','CFC700','CFC700','CF1100','CF00BE','F00');
     $user_colour = array_rand($colours);
@@ -111,7 +124,29 @@
     <script src="jquery-3.1.1.js"></script>
 
 
-    <script language="javascript" type="text/javascript">  
+    <script language="javascript" type="text/javascript"> 
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        var ballRadius = 10;
+        var x = canvas.width/2; // start off possition.
+        var y = canvas.height-70; // start off position
+        var dx = 5; // control gravity / dropping angel and speed
+        var dy = -5;// control gravity / dropping angel and speed
+        var paddleHeight = 10;
+        var paddleWidth = 75;
+        var paddleX = (canvas.width-paddleWidth)/2;//space outside the paddle in half
+        var rightPressed = false;//mouse will move to right automatically when user is not controlling
+        var leftPressed = false;//mouse will move to left automatically when user is not controlling.
+        var brickRowCount = 14;
+        var brickColumnCount = 8;
+        var brickWidth = 60; 
+        var brickHeight = 15; 
+        var brickPadding = 8;//gap in between each brick
+        var brickOffsetTop = 30;
+        var brickOffsetLeft = 10;
+        var score = 0;
+        var lives = 3;
+
     $(document).ready(function(){
         //create a new WebSocket object.
         var wsUri = "ws://localhost:9000/demo/server.php";  
@@ -121,9 +156,20 @@
             $('#message_box').append("<div class=\"system_msg\">Connected!</div>"); //notify user
         }
 
+        <?php echo "var myname = '" .$name . "'"; ?>
+
+        function sendScore() {
+            var msg = {
+                message: score,
+                name: myname,
+                color : '<?php echo $colours[$user_colour]; ?>'
+                };
+                //convert and send data to server
+                websocket.send(JSON.stringify(msg));
+            }
+
         $('#send-btn').click(function(){ //use clicks message send button   
             var mymessage = $('#message').val(); //get message text
-            var myname = $('#name').val(); //get user name
             
             if(myname == ""){ //empty name?
                 alert("Enter your Name please!");
@@ -133,10 +179,12 @@
                 alert("Enter Some message Please!");
                 return;
             }
-            document.getElementById("name").style.visibility = "hidden";
+            document.getElementById("name").style.visibility = "hidden"
+
             
             var objDiv = document.getElementById("message_box");
             objDiv.scrollTop = objDiv.scrollHeight;
+
             //prepare json data
             var msg = {
             message: mymessage,
@@ -172,45 +220,10 @@
         
         websocket.onerror   = function(ev){$('#message_box').append("<div class=\"system_error\">Error Occurred - "+ev.data+"</div>");}; 
         websocket.onclose   = function(ev){$('#message_box').append("<div class=\"system_msg\">Connection Closed</div>");}; 
-    });
-
-    </script>
-    
-    <div class="chat_wrapper">
-    <div class="message_box" id="message_box"></div>
-    <div class="panel">
-    <input type="text" name="name" id="name" placeholder="Your Name" maxlength="15" />
-
-    <input type="text" name="message" id="message" placeholder="Message" maxlength="80" 
-    onkeydown = "if (event.keyCode == 13)document.getElementById('send-btn').click()"  />
-
-    </div>
-        <button id="send-btn" class=button>Send</button>
-    </div>
+   
 
 
-    <script>
-        var canvas = document.getElementById("myCanvas");
-        var ctx = canvas.getContext("2d");
-        var ballRadius = 10;
-        var x = canvas.width/2; // start off possition.
-        var y = canvas.height-70; // start off position
-        var dx = 5; // control gravity / dropping angel and speed
-        var dy = -5;// control gravity / dropping angel and speed
-        var paddleHeight = 10;
-        var paddleWidth = 75;
-        var paddleX = (canvas.width-paddleWidth)/2;//space outside the paddle in half
-        var rightPressed = false;//mouse will move to right automatically when user is not controlling
-        var leftPressed = false;//mouse will move to left automatically when user is not controlling.
-        var brickRowCount = 14;
-        var brickColumnCount = 8;
-        var brickWidth = 60; 
-        var brickHeight = 15; 
-        var brickPadding = 8;//gap in between each brick
-        var brickOffsetTop = 30;
-        var brickOffsetLeft = 10;
-        var score = 0;
-        var lives = 3;
+        
 
         var bricks = [];
         for(c=0; c<brickColumnCount; c++) {
@@ -255,6 +268,7 @@
                             dy = -dy; // control the ball to go the oposit direction
                             b.status = 0;
                             score++;
+                            sendScore();
                             if(score == brickRowCount*brickColumnCount) { //every time you hit a brick, your score increase by 1.
                                 alert("YOU WIN, CONGRATS!");
                                 document.location.reload();
@@ -354,6 +368,8 @@
             requestAnimationFrame(draw);
         }
         draw();
+
+     });
     </script>
 
     <?php
@@ -407,7 +423,6 @@
             src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
         <script type="text/javascript">
     </script>
-
 
 </body>
 </html>
